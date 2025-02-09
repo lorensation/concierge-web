5fb8d16876117fbe2c1b6153d50ccdc7117ccdb5
lorensation <lsanztrucharte@gmail.com>
2025-02-09 23:36:09 +0100

    ruta relativa bookingcalendar corregida

--- src/components/full/BookingSection.jsx
+++ src/components/full/BookingSection.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import BookingCalendar from "./BookingCalendar";
+import BookingCalendar from "../BookingCalendar";
 
 export default function BookingSection() {
   const [selectedSlot, setSelectedSlot] = useState(null);
